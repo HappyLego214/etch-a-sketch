53b927d1daf9615ccc7a0950271111453fe56f8a
HappyLego214 <anthony21josh@gmail.com>
2023-02-25 15:22:32 +0800

    refactored grid cleaning

--- scripts.js
+++ scripts.js
@@ -1,38 +1,59 @@
-const grids = document.querySelectorAll('.grid-options');
+const gridOptions = document.querySelectorAll('.grid-options');
 const gridContainer = document.querySelector('.grid-container');
+const cleanButton = document.querySelector("#cleanTool");
+const gridColumns = document.getElementById("g-container");
+const gridRows = document.getElementById("g-container");
+ 
+window.addEventListener('load', () => {
+    firstOption();
+});
 
-grids.forEach((grid) => {
-    grid.addEventListener('click', event => {
+gridOptions.forEach((grid) => {
+    grid.addEventListener('click', () => {
         if (grid.id == "one") {
-            for (i = 0; i < 225; i++) {
-                clearGrid()
-                document.getElementById("g-container").style.gridTemplateColumns = "repeat(15, minmax(1px, 1fr))";
-                document.getElementById("g-container").style.gridTemplateRows = "repeat(15, minmax(1px, 1fr));"
-                const divGrid = document.createElement('div');
-                divGrid.classList.add('cell');
-                gridContainer.appendChild(divGrid);
-            }
-
+            firstOption();
         } else if (grid.id == "two") {
-            for (i = 0; i < 900; i++) {
-                clearGrid()
-                document.getElementById("g-container").style.gridTemplateColumns = "repeat(30, minmax(1px, 1fr))";
-                document.getElementById("g-container").style.gridTemplateRows = "repeat(30, minmax(1px, 1fr));"
-                const divGrid = document.createElement('div');
-                divGrid.classList.add('cell');
-                gridContainer.appendChild(divGrid);
-            }
-
+            secondOption();
         } else if (grid.id == "three") {
-            for (i = 0; i < 3600; i++) {
-                clearGrid()
-                document.getElementById("g-container").style.gridTemplateColumns = "repeat(60, minmax(1px, 1fr))";
-                document.getElementById("g-container").style.gridTemplateRows = "repeat(60, minmax(1px, 1fr));"
-                const divGrid = document.createElement('div');
-                divGrid.classList.add('cell');
-                gridContainer.appendChild(divGrid);
-                
-            }
+            thirdOption();
         }
     });
 });
+
+function firstOption() {
+    gridContainer.replaceChildren();
+    for (i = 0; i < 225; i++) {
+        gridColumns.style.gridTemplateColumns = "repeat(15, minmax(1px, 1fr))";
+        gridRows.style.gridTemplateRows = "repeat(15, minmax(1px, 1fr));"
+        const divGrid = document.createElement('div');
+        divGrid.classList.add('cell');
+        divGrid.setAttribute('id', 'gridCell')
+        gridContainer.appendChild(divGrid);
+    }
+}
+
+function secondOption() {
+    gridContainer.replaceChildren();
+    for (i = 0; i < 900; i++) {;
+        gridColumns.style.gridTemplateColumns = "repeat(30, minmax(1px, 1fr))";
+        gridRows.style.gridTemplateRows = "repeat(30, minmax(1px, 1fr));"
+        const divGrid = document.createElement('div');
+        divGrid.classList.add('cell');
+        divGrid.setAttribute('id', 'gridCell')
+        gridContainer.appendChild(divGrid);
+    }
+}
+
+function thirdOption() {
+    gridContainer.replaceChildren();;
+    for (i = 0; i < 3600; i++) {
+        gridColumns.style.gridTemplateColumns = "repeat(60, minmax(1px, 1fr))";
+        gridRows.style.gridTemplateRows = "repeat(60, minmax(1px, 1fr));"
+        const divGrid = document.createElement('div');
+        divGrid.classList.add('cell');
+        divGrid.setAttribute('id', 'gridCell')
+        gridContainer.appendChild(divGrid);
+        
+    }
+}
+
